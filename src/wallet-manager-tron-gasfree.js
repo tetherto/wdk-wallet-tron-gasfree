a9ee787e650aee246ae1756c964022778095ec5a
NPC <nulllpc@pm.me>
2026-09-24 15:09:14 +0700

    docs(wdk-wallet-tron-gasfree): clarify seed param accepts raw BIP-32 bytes, not just BIP-39
    - The wallet manager and account constructor JSDoc described seed as
      "The wallet's BIP-39 seed phrase," mislabeling the Uint8Array case
      as BIP-39 material.
    - The Uint8Array case is a raw BIP-32 master seed, same fix as
      tetherto/wdk-wallet#74.

--- src/wallet-manager-tron-gasfree.js
+++ src/wallet-manager-tron-gasfree.js
@@ -30,7 +30,7 @@ export default class WalletManagerTronGasfree extends WalletManager {
   /**
    * Creates a new wallet manager for the tron blockchain that implements gasfree features.
    *
-   * @param {string | Uint8Array} seed - The wallet's [BIP-39](https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki) seed phrase.
+   * @param {string | Uint8Array} seed - A [BIP-39](https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki) mnemonic seed phrase, or a raw BIP-32 master seed (16-64 bytes).
    * @param {TronGasfreeWalletConfig} config - The configuration object.
    */
   constructor (seed, config) {
